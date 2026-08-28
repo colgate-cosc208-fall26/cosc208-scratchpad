17460cf50572a008a1a8723d5f6e00bb4824f006
jsommers <jsommers@colgate.edu>
2026-08-28 11:25:45 -0400

    update week00

--- week00/workspace.c
+++ week00/workspace.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-
 int main() {
-        int days = 7;
-        days *= 2;
-        int hours = days * 24;
-        int minutes = hours * 60;
-        printf("%d days %d hours %d minutes in a week\n", days, hours, minutes);
-        return 0;
+   char first = 'J';
+   char last = 'S';    
+   int systolic = 108, diastolic = 60;
+   printf("%c.%c. %d/%d\n", first, last, systolic, diastolic);
+   return 0;
 }
